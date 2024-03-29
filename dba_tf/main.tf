0755545c4475444677e1e2b628a6e20a65e75187
waheednazeer <waheed.nazeer@hotmail.com>
2024-03-29 10:30:01 +0500

    authorized network

--- dba_tf/main.tf
+++ dba_tf/main.tf
@@ -1,27 +1,32 @@
 provider "google" {
-    project = "database-adminstration"
     region = "us-central1"
     zone = "us-central1-a"
 }
 
-resource "google_sql_database" "database_label_terraform" {
-    name = "my-postgres-database"
-    instance = google_sql_database_instance.instance_label_terraform.name
+resource "google_sql_database" "database_postgres" {
+    name = "postgres-db"
+    instance = google_sql_database_instance.instance_postgres.name
 }
 
-resource "google_sql_database_instance" "instance_label_terraform" {
+resource "google_sql_database_instance" "instance_postgres" {
     name = "postgres-instance"
     region = "us-central1"
-    database_version = "POSTGRES_11"
+    database_version = "POSTGRES_15"
     settings {
       tier = "db-f1-micro"
+      ip_configuration {
+      authorized_networks {
+        name            = "postgres-net"
+        value           = "0.0.0.0/0"
+      }
+      }
     }
     deletion_protection = "false"  
 }
 /* First database resources*/
-resource "google_sql_user" "users" {
-    name = "postgre-dev-user"
-    instance = google_sql_database_instance.instance_label_terraform.name
+resource "google_sql_user" "user1" {
+    name = "postgre-user"
+    instance = google_sql_database_instance.instance_postgres.name
     password = "Passw0rd!"
   }
   
